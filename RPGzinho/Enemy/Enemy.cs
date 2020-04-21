8adec7f45cdf6e143a2a60eeadc2a0af1a2b040d
Gustavo Felski <gustavo_felski@hotmail.com>
2020-04-21 12:51:58 +0100

    Eneny added

--- RPGzinho/Enemy/Enemy.cs
+++ RPGzinho/Enemy/Enemy.cs
@@ -2,12 +2,57 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace RPGzinho.Enemy
+namespace RPGzinho.Character
 {
-    class Enemy
+    public class Enemy
     {
-        private double Damage;
+        private string Name;
+        private double Life;
+        private double Resistence;
+        private int Speed;
         private double CriticalChance;
-        
+        private double CriticalDamage;
+        Enemy(int type) {
+            if(type == 1)
+            {
+                this.Name = "Goblin";
+                this.Life = 50;
+                this.Resistence = 10;
+                this.Speed = 0;
+                this.CriticalChance = 20;
+            }
+            if (type == 2)
+            {
+                this.Name = "Orc";
+                this.Life = 125;
+                this.Resistence = 30;
+                this.Speed = -3;
+                this.CriticalChance = 0;
+            }
+            if (type == 3)
+            {
+                this.Name = "Werewolf";
+                this.Life = 150;
+                this.Resistence = 20;
+                this.Speed = -2;
+                this.CriticalChance = 20;
+            }
+
+
+        } 
+        public void LightAttack()
+        {
+
+        }
+        public void HeavyAttack()
+        {
+
+        }
+
+        public void Defence()
+        {
+
+        }
     }
+
 }
---
+++ RPGzinho/StoryTeller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPGzinho
+{
+    public class StoryTeller
+    {
+        //Called a mensage from combate
+
+
+    }
+}

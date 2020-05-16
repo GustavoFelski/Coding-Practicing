ed8ae5704a8c48a3cf719e1946ffacbac35cc3de
Gustavo Felski <gustavo_felski@hotmail.com>
2020-05-16 13:25:54 +0100

    classes rearranged
    I change the classes to match with SOLID Consept SRP, the classes and enemy will have a file with his atributes, I will also change some classes, and create a interfaces between than

--- RPGzinho/Character/Character.cs
+++ RPGzinho/Character/Character.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace RPGzinho.Character
 {
+    /// <summary>
+    /// Class Character attributes and Attacks
+    /// </summary>
     public class Character
     {
         private string Name;
@@ -12,81 +15,9 @@ namespace RPGzinho.Character
         private double Resistence;
         private double Damage;
         private int Potions;
-        private int MP;
+        private int MagicPoits;
         private int Speed;
         private double CriticalChance;
-        private string HeavyAttackName;
-
-        #region Gets and Sets
-        public int MPupdade { get; set; }
-        public string CharacterName
-        {
-            get { return Name; }
-            set { Name = value; }
-        }
-        public string ClassCharacter {
-            get { return Classe; }
-            set { Classe = value; }
-        }
-        public double LifeUpdate
-        {
-            get { return Life; }
-            set { Life = value; }
-        }
-        public double ResistenceUpdate
-        {
-            get { return Resistence; }
-            set { Resistence = value; }
-        }
-        public int SpeedUpdate
-        {
-            get { return Speed; }
-            set { Speed = value; }
-        }
-        public double CriticalChanceUpdate
-        {
-            get { return CriticalChance; }
-            set { CriticalChance = value; }
-        }
-        public int PotionsUpdate { get; set; }
-        #endregion
-        public double LightAttack()
-        {
-            double DamageAttack;
-            Random crit = new Random();
-            if (crit.Next(0, 100) <= CriticalChance)
-                return DamageAttack = (Damage / 2) + ((Damage * (CriticalChance / 2)) / 100);
-            else
-                return DamageAttack = (Damage / 2);
-        }
-        public double HeavyAttack() {
-            double DamageAttack;
-            Random crit = new Random();
-            if (crit.Next(0, 100) <= CriticalChance)
-                return DamageAttack = Damage + ((Damage * CriticalChance) / 100);
-            else
-                return DamageAttack = Damage;
-        }
-
-        public void UsePotionMP() {
-            if (this.ClassCharacter == "warrior") MP = 2;
-            if (this.ClassCharacter == "mage") MP = 6;
-            if (this.ClassCharacter == "ladino") MP =4;
-        }
-
-        public void Defence(double attack) {
-
-            Random crit = new Random();
-            if (crit.Next(0, 100) <= CriticalChance)
-                Life -= (attack - Resistence);
-            else
-                Life -= (attack - Resistence / 2);
-        }
-        public void UsePotionLife () {
-            if (this.ClassCharacter == "warrior") Life = 150;
-            if (this.ClassCharacter == "mage") Life = 50;
-            if (this.ClassCharacter == "ladino") Life = 100;
-        }
 
     }
 }
